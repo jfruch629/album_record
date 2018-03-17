1fb3ffd142aa16e7177eb68c28e563893d6d9e04
vagrant <vagrant@precise64.(none)>
2018-03-17 15:40:54 +0000

    add image controller

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,4 +1,4 @@
-class ImageController < ApplicationController
+class ImagesController < ApplicationController
   def new
     @uploader = Album.new.image
     @uploader.success_action_redirect = new_album_path
