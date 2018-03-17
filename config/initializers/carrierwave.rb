cc2271674f88b6b984d811066684c628be854d95
vagrant <vagrant@precise64.(none)>
2018-03-17 15:03:29 +0000

    aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,12 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AKIAJTUQRT5QR6CTRFMA'],
-      :aws_secret_access_key  => ENV['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'],     # required
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
-    config.storage = :aws
-    config.aws_directory  = ENV['recordalbumbucket']                 # required
-    config.aws_public     = true                                   # optional, defaults to true
-    config.root = Rails.root.join('tmp')
-    config.cache_dir = '#{Rails.root}/tmp/uploads'
-    config.permissions = 0777
-    config.aws_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-  end
-else
-  CarrierWave.configure do |config|
-    config.storage :file
+    config.fog_directory  =  ENV['S3_BUCKET']
   end
 end
