4dff390731db41546c87f3ddca8b11426f85a7a5
vagrant <vagrant@precise64.(none)>
2018-03-17 20:29:24 +0000

    small fix

--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -1,5 +1,4 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     'AKIAI53DFWDU22POTZTQ',                        # required
@@ -8,6 +7,9 @@ CarrierWave.configure do |config|
     host:                  's3.example.com',             # optional, defaults to nil
     endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
+
+  config.fog_provider = 'fog'                        # required
+
   config.fog_directory  = 'album-records-production'                                   # required
   config.fog_public     = false                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
