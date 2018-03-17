bbbff6200e3ba493950adcd4d0b496f114530854
vagrant <vagrant@precise64.(none)>
2018-03-17 20:24:02 +0000

    this has to finish carrierwave + s3 integraton

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
   CarrierWave.configure do |config|
     if !Rails.env.test?
     config.fog_credentials = {
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
     if Rails.env.production?
      config.fog_directory  = ENV["PRODUCTION_S3_BUCKET"]
