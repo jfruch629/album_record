e5b407f1dc899e6293df304a65d8b95b2a069ead
vagrant <vagrant@precise64.(none)>
2018-03-17 15:09:28 +0000

    fix credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJHOGTRGBTEGN2AUA'],
-      :aws_secret_access_key => ENV['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'],
-      :region                => ENV['us-east-2']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
-    config.fog_directory  =  ENV['recordalbumbucket']
+    config.fog_directory  =  ENV['S3_BUCKET']
   end
 end
