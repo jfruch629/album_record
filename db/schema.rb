257bdb005f9f49d855aac5ec2ce26dba0fcab91e
vagrant <vagrant@precise64.(none)>
2018-03-19 19:33:34 +0000

    improved css. added album timestamps too.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317171027) do
+ActiveRecord::Schema.define(version: 20180319190139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20180317171027) do
     t.string "tracks", default: [], array: true
     t.integer "user_id"
     t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
